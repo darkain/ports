26253b7814406c2c4dc417796e5a695e2bf17756
Vince <git@darkain.com>
2022-07-06 20:20:36 -0700

    Adding MariaDB 10.9

---
+++ databases/mariadb109-server/files/patch-mysys_crc32_crc32c.cc
@@ -0,0 +1,24 @@
+--- mysys/crc32/crc32c.cc.orig	2022-05-18 09:04:51 UTC
++++ mysys/crc32/crc32c.cc
+@@ -527,6 +527,21 @@ static int arch_ppc_probe(void) {
+ 
+   return arch_ppc_crc32;
+ }
++#elif __FreeBSD__
++#include <machine/cpu.h>
++#include <sys/auxv.h>
++#include <sys/elf_common.h>
++static int arch_ppc_probe(void) {
++  unsigned long cpufeatures;
++  arch_ppc_crc32 = 0;
++
++#if defined(__powerpc64__)
++  elf_aux_info(AT_HWCAP2, &cpufeatures, sizeof(cpufeatures));
++  if (cpufeatures & PPC_FEATURE2_HAS_VEC_CRYPTO) arch_ppc_crc32 = 1;
++#endif  /* __powerpc64__ */
++
++  return arch_ppc_crc32;
++}
+ #endif  // __linux__
+ #endif
+ 
